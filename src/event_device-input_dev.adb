fadf6223082c233e117a481fa78952922d1b747b
onox <denkpadje@gmail.com>
2021-04-19 02:27:49 +0200

    Move some functions to body of package Event_Device.Input_Dev
    Signed-off-by: onox <denkpadje@gmail.com>

--- src/event_device-input_dev.adb
+++ src/event_device-input_dev.adb
@@ -27,14 +27,44 @@ package body Event_Device.Input_Dev is
       Count           : Size_Type) return Signed_Size_Type
    with Import, Convention => C, External_Name => "write";
 
-   function C_Close
-     (File_Descriptor : Interfaces.C.int) return Interfaces.C.int
-   with Import, Convention => C, External_Name => "close";
-
    function C_Open
      (Path_Name : String;
       Flags     : Access_Flag) return int
    with Import, Convention => C, External_Name => "open";
+
+   function C_Close
+     (File_Descriptor : Interfaces.C.int) return Interfaces.C.int
+   with Import, Convention => C, External_Name => "close";
+
+   function C_Ioctl
+     (FD      : int;
+      Request : unsigned_long;
+      Value   : System.Address) return int
+   with Import, Convention => C, External_Name => "ioctl";
+
+   function C_Ioctl
+     (FD      : int;
+      Request : unsigned_long;
+      Value   : int) return int
+   with Import, Convention => C, External_Name => "ioctl";
+
+   function Convert is new Ada.Unchecked_Conversion
+     (Source => IOCTL_Command,
+      Target => unsigned);
+
+   ----------------------------------------------------------------------------
+
+   function IO_Control
+     (FD      : File_Descriptor;
+      Command : IOCTL_Command;
+      Value   : System.Address) return Integer
+   is (Integer (C_Ioctl (int (FD), unsigned_long (Convert (Command)), Value)));
+
+   function IO_Control
+     (FD      : File_Descriptor;
+      Command : IOCTL_Command;
+      Value   : Integer) return Integer
+   is (Integer (C_Ioctl (int (FD), unsigned_long (Convert (Command)), int (Value))));
 
    ----------------------------------------------------------------------------
 
--- src/event_device-input_dev.ads
+++ src/event_device-input_dev.ads
@@ -74,32 +74,4 @@ private
    end record;
    for IOCTL_Command'Size use 32;
 
-   function ioctl
-     (FD      : int;
-      Request : unsigned_long;
-      Value   : System.Address) return int
-   with Import, Convention => C, External_Name => "ioctl";
-
-   function ioctl
-     (FD      : int;
-      Request : unsigned_long;
-      Value   : int) return int
-   with Import, Convention => C, External_Name => "ioctl";
-
-   function Convert is new Ada.Unchecked_Conversion
-     (Source => IOCTL_Command,
-      Target => unsigned);
-
-   function IO_Control
-     (FD      : File_Descriptor;
-      Command : IOCTL_Command;
-      Value   : System.Address) return Integer
-   is (Integer (ioctl (int (FD), unsigned_long (Convert (Command)), Value)));
-
-   function IO_Control
-     (FD      : File_Descriptor;
-      Command : IOCTL_Command;
-      Value   : Integer) return Integer
-   is (Integer (ioctl (int (FD), unsigned_long (Convert (Command)), int (Value))));
-
 end Event_Device.Input_Dev;
